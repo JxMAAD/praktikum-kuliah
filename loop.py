385adfed3f237bd3a1fef844d379ac2f60a9f70a
MiloxCat06 <lalxmilo0607@gmail.com>
2025-09-16 10:12:57 +0700

    new update

---
+++ loop.py
@@ -0,0 +1,22 @@
+i = 14
+
+while i >= -16:
+    print(i, end = " ")
+    i -= 6
+
+daftar = ['Matematika', 'Fisika', 'Statistika']
+
+for i in range(len(daftar)):
+    print("%d: %s" % (i+1, daftar[i]))
+
+for i in range(1,11):
+    for j in range(1, i+1):
+        print("%d " % (i*j), end='')
+    print()
+
+i = 1
+while i <=10:
+    for j in range(1, i+1):
+        print("%d " % (i*j), end=' ')
+    print()
+    i+=1
